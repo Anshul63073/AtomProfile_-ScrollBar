24b6674e6d49c573d28dfe8c8b0bed680f406368
Anshul63073 <anshulgajariya@gmail.com>
2023-05-12 10:32:38 +0530

    Scroller

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// // import ProfileCard from './ProfileCard';
+// // import ScrollBar from './ScrollBar';
+
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <ProfileCard />
+//       <ScrollBar /> */}
+//     </div>
+//   );
+// };
+
+// export default App;
